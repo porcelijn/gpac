porcelijn/gpac

commit bead97ffc7b069bfb3d5c2ef4db89205d3fcf2d2

Added warning+patch for negative stts entries

src/isomedia/box_code_base.c
@@ -5759,6 +5759,9 @@ GF_Err stts_Read(GF_Box *s, GF_BitStream *bs)
 				GF_LOG(GF_LOG_WARNING, GF_LOG_CONTAINER, ("[iso file] more than one sample at the end of the track with sample_delta=0 - forbidden ! Fixing to 1\n" ));
 				ptr->entries[i].sampleDelta = 1;
 			}
+		} else if ((s32) ptr->entries[i].sampleDelta < 0) {
+			GF_LOG(GF_LOG_WARNING, GF_LOG_CONTAINER, ("[iso file] stts entry %d has negative duration %d - forbidden ! Fixing to 1, sync may get lost (consider reimport raw media)\n", i, (s32) ptr->entries[i].sampleDelta ));
+			ptr->entries[i].sampleDelta = 1;
 		}
 	}
 	//remove the last sample delta.

commit 695ce1f3b76579ddcc4f9801239a7fb9d8636d1c

Fixed handling of large `sample_delta` values TimeToSampleBox(`stts`)  - incorrect handling introduced in: https://github.com/gpac/gpac/commit/bead97ffc7b069bfb3d5c2ef4db89205d3fcf2d2

src/isomedia/box_code_base.c
@@ -6355,8 +6355,7 @@ GF_Err stts_Read(GF_Box *s, GF_BitStream *bs)
 				ptr->entries[i].sampleDelta = 1;
 			}
 		} else if ((s32) ptr->entries[i].sampleDelta < 0) {
-			GF_LOG(GF_LOG_WARNING, GF_LOG_CONTAINER, ("[iso file] stts entry %d has negative duration %d - forbidden ! Fixing to 1, sync may get lost (consider reimport raw media)\n", i, (s32) ptr->entries[i].sampleDelta ));
-			ptr->entries[i].sampleDelta = 1;
+			GF_LOG(GF_LOG_WARNING, GF_LOG_CONTAINER, ("[iso file] stts entry %d has HUGE duration %d\n", i, (s32) ptr->entries[i].sampleDelta ));
 		}
 	}
 	if (ptr->size<(ptr->nb_entries*8)) return GF_ISOM_INVALID_FILE;